de66a51e44e66121f9fe97292627a03614edd8b8
Jongkwan Park <jkff2123@outlook.kr>
2019-09-09 18:15:17 +0200

    Initial commit

---
+++ WeatherSource/IWeatherSource.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JKWeatherAPI.WeatherSource
+{
+    public interface IWeatherSource
+    {
+        CurrentWeather GetCurrentWeather(float latitude, float longitude);
+        WeeklyWeather GetWeeklyWeather(float latitude, float longitude);
+    }
+
+    public class WeatherInfo
+    {
+        private string _state;
+        private float _tempCurrent;
+        private float _tempMax;
+        private float _tempMin;
+        private float _windSpeed;
+        private float _windDegree;
+
+        public string State { get { return _state; } set { _state = value; } }
+        public float TempCurrent { get { return _tempCurrent; } set { _tempCurrent = value; } }
+        public float TempMax { get { return _tempMax; } set { _tempMax = value; } }
+        public float TempMin { get { return _tempMin; } set { _tempMin = value; } }
+        public float WindSpeed { get { return _windSpeed; } set { _windSpeed = value; } }
+        public float WindDegree { get { return _windDegree; } set { _windDegree = value; } }
+
+        public WeatherInfo(string state, float tempCurrent, float tempMax = 0.0f, float tempMin = 0.0f, float windSpeed = 0.0f, float windDegree = 0.0f)
+        {
+            _state = state;
+            _tempCurrent = tempCurrent;
+            _tempMax = tempMax;
+            _tempMin = tempMin;
+            _windSpeed = windSpeed;
+            _windDegree = windDegree;
+        }
+    }
+
+    public class CurrentWeather
+    {
+        private string _location;
+        private string _country;
+        private WeatherInfo _weatherInfo;
+
+        public string Location { get { return _location; } set { _location = value; } }
+        public string Country { get { return _country; } set { _country = value; } }
+        public WeatherInfo Weather { get { return _weatherInfo; } set { _weatherInfo = value; } }
+
+        public CurrentWeather(string location, string country, WeatherInfo weatherInfo)
+        {
+            _location = location;
+            _country = country;
+            _weatherInfo = weatherInfo;
+        }
+    }
+
+    public class WeeklyWeather
+    {
+        private string _location;
+        private string _country;
+        private WeatherInfo[] _weatherInfo = new WeatherInfo[7];
+
+        public string Location { get { return _location; } set { _location = value; } }
+        public string Country { get { return _country; } set { _country = value; } }
+        public WeatherInfo[] Weather { get { return _weatherInfo; } set { _weatherInfo = value.Length == 7 ? value : null; } }
+
+        public WeeklyWeather(string location, string country, WeatherInfo[] weatherInfo)
+        {
+            _location = location;
+            _country = country;
+            _weatherInfo = weatherInfo.Length == 7 ? weatherInfo : null;
+        }
+    }
+}
---
+++ WeatherSource/OpenWeatherAPI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace JKWeatherAPI.WeatherSource
+{
+    public class OpenWeatherAPI : IWeatherSource
+    {
+        private void RequestToServer(string url)
+        {
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+
+            var responseStream = response.GetResponseStream();
+            var readStream = new StreamReader(responseStream);
+            var read = readStream.ReadToEnd();
+        }
+
+        public CurrentWeather GetCurrentWeather(float latitude, float longitude)
+        {
+            var currentWeatherUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={ latitude.ToString() }&lon={ longitude.ToString() }&appid=cb0b1a243a5d3f3f6cef60b1497ccf19";
+            RequestToServer(currentWeatherUrl);
+
+            return new CurrentWeather("", "", new WeatherInfo("",10.0f));
+        }
+
+        public WeeklyWeather GetWeeklyWeather(float latitude, float longitude)
+        {
+            var weeklyWeatherUrl = $"https://api.openweathermap.org/data/2.5/forecast/daily?lat={ latitude.ToString() }&lon={ longitude.ToString() }&cnt=7&appid=cb0b1a243a5d3f3f6cef60b1497ccf19";
+            RequestToServer(weeklyWeatherUrl);
+
+            return new WeeklyWeather("", "", new WeatherInfo[7]);
+        }
+    }
+}
